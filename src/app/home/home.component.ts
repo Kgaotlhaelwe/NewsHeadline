9d036667fd12bcb9f59c629e1b772a3c4d674fe0
unknown <Kgaotlhaelwe@gmaial.com>
2020-04-20 07:17:30 +0200

    fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewChild , ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import * as moment from 'moment'
+import Swal from 'sweetalert2'
+
 
 @Component({
   selector: 'app-home',
@@ -12,11 +14,16 @@ export class HomeComponent implements OnInit {
   articles = [];
   searchKeyword = [];
   showStories: boolean = false;
-  future = "2020-04-19";
+
+
   d = new Date();
   datez = moment(this.d).format().substring(0, 10);;
   readmore;
 
+ 
+@ViewChild("phrase") keywor: ElementRef
+
+ 
   constructor(private router: Router) {
 
 
@@ -26,19 +33,22 @@ export class HomeComponent implements OnInit {
 
 
   }
-  Search(keyword, date) {
-    this.showStories = true;
-    const url = `https://newsapi.org/v2/everything?q=${keyword}&from=${date}&apiKey=daebbb953e894759ae7a5a0e4dea55cb`
-    fetch(url)
-      .then(response => response.json())
-      .then((contents: any) => {
-        console.log(contents.articles)
-        this.articles = contents.articles;
-        keyword = "";
-        date = "";
+  Search(word, date) {
+    if (word != "") {
+      this.showStories = true;
+      const url = `https://newsapi.org/v2/everything?q=${word}&from=${date}&apiKey=daebbb953e894759ae7a5a0e4dea55cb`
+      fetch(url)
+        .then(response => response.json())
+        .then((contents: any) => {
+          console.log(contents.articles)
+          this.articles = contents.articles;
+         })
+        .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 
-      })
-      .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+    } else {
+      Swal.fire('Oops...', 'Something went wrong!', 'error')
+    }
+
   }
 
   readMore(article) {
